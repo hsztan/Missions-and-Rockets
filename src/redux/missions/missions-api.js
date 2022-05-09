22b8614c750127b7ba4a1b0e0c9e5017ee598481
Henry Nawrocki <hdev@sztanski.com>
2022-05-09 16:31:51 -0500

    Add fetch rockets and missions for reducers initial state

--- src/redux/missions/missions-api.js
+++ src/redux/missions/missions-api.js
@@ -1,1 +1,13 @@
 const missionsEndpoint = 'https://api.spacexdata.com/v3/missions';
+
+export const fetchMissions = async () => {
+  try {
+    const res = await fetch(missionsEndpoint);
+    const rockets = await res.json();
+    if (res.ok) {
+      return rockets;
+    }
+  } catch (error) {
+    return console.log(error.message);
+  }
+};
