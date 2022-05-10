021b403e87b775336f54f750f4e33e04e5ef16d7
Henry Nawrocki <hdev@sztanski.com>
2022-05-10 14:23:47 -0500

    Add user reducer

---
+++ src/redux/user/user.js
@@ -0,0 +1,49 @@
+const RESERVE_ROCKET = 'spaceTravel/user/RESERVE_ROCKET';
+const REMOVE_ROCKET = 'spaceTravel/user/REMOVE_ROCKET';
+const JOIN_MISSION = 'spaceTravel/user/JOIN_MISSION';
+const LEAVE_MISSION = 'spaceTravel/user/LEAVE_MISSION';
+
+const initialState = { rockets: [], missions: [] };
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case RESERVE_ROCKET:
+      return { ...state, rockets: [...state.rockets, action.payload] };
+    case REMOVE_ROCKET:
+      return {
+        ...state,
+        rockets: state.rockets.filter((rocket) => rocket.id !== action.payload),
+      };
+    case JOIN_MISSION:
+      return { ...state, missions: [...state.missions, action.payload] };
+    case LEAVE_MISSION:
+      return {
+        ...state,
+        missions: state.missions.filter(
+          (mission) => mission.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+}
+
+export const reserveRocket = (rocket) => ({
+  type: RESERVE_ROCKET,
+  payload: rocket,
+});
+
+export const removeRocket = (id) => ({
+  type: REMOVE_ROCKET,
+  payload: id,
+});
+
+export const joinMission = (mission) => ({
+  type: JOIN_MISSION,
+  payload: mission,
+});
+
+export const leaveMission = (id) => ({
+  type: LEAVE_MISSION,
+  payload: id,
+});
