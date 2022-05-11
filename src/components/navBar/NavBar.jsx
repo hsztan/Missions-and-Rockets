3dd62354572dd08bafa40d8d2e774e9957088232
Henry Nawrocki <hdev@sztanski.com>
2022-05-10 19:40:08 -0500

    Change Link to NavLink components for active styling

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logoImage from '../../assets/images/logo.png';
 import './NavBar.style.scss';
 
 const NavBar = () => (
   <nav className="navbar">
     <div className="navbar-container">
-      <Link to="/" className="navbar-brand">
+      <NavLink to="/" className="navbar-brand">
         <img src={logoImage} alt="logo" />
         <p>Space Travelers&apos; Hub</p>
-      </Link>
-      <div className="navbar-links">
-        <Link to="/"> Rockets </Link>
-        <Link to="/missions"> Missions </Link>
+      </NavLink>
+      <div className="navbar-NavLinks">
+        <NavLink to="/"> Rockets </NavLink>
+        <NavLink to="/missions"> Missions </NavLink>
         <p>|</p>
-        <Link to="/profile"> My Profile </Link>
+        <NavLink to="/profile"> My Profile </NavLink>
       </div>
     </div>
   </nav>
